b6c92b9d62437e515f9e3d4417a4dc9620969fd0
xmeadow <crossmeadow@gmail.com>
2026-04-08 00:05:21 +0200

    add version and copyright to main menu

--- common.mk
+++ common.mk
@@ -4,6 +4,12 @@ SDP = sdp/sdp
 SLC = slc/slc
 SSC = ssc/ssc
 
+GIT_REMOTE_URL := $(shell git remote get-url origin 2>/dev/null | sed 's|^https://||;s|^git@\([^:]*\):\(.*\)|\1/\2|;s|\.git$$||' || echo unknown)
+BUILD_YEAR     := $(shell date +%Y)
+GIT_TAG        := $(shell git tag --sort=-creatordate 2>/dev/null | head -1 || echo unknown)
+GIT_HASH       := $(shell git rev-parse --short HEAD 2>/dev/null || echo unknown)
+GIT_VERSION    := $(GIT_TAG) $(GIT_HASH)
+
 DATAFILE = datafile.sdf
 DATADIR = datadir
 
@@ -19,7 +25,7 @@ DATADEP = $(foreach ext,$(DATAEXT),$(wildcard $(DATADIR)/*.$(ext))) $(LANFILES) 
 data: $(DATAFILE)
 
 clean:
-	rm -rf $(TARGET) $(TARGET).exe $(DATAFILE) $(LANFILES) $(SCRIPTRUN) resource.res
+	rm -rf $(TARGET) $(TARGET).exe $(DATAFILE) $(LANFILES) $(SCRIPTRUN) $(DATADIR)/WMAIN.SRC.RUN resource.res
 
 # TBD: ssc should compile only things that changed since last build
 $(DATAFILE): $(DATADEP) | $(SDP)
@@ -29,7 +35,18 @@ $(DATADIR)/%.LAN: language/%.txt | $(SLC)
 	$(SLC) --to-lan $< -o $@
 
 $(SCRIPTRUN): $(SCRIPTSRC) | $(SSC)
-	$(SSC) -c -i $(DATADIR)
+	@ORIG_MTIME=$$(stat -c %y $(DATADIR)/WMAIN.SRC); \
+	TMPBAK=$$(mktemp); \
+	cp $(DATADIR)/WMAIN.SRC $$TMPBAK; \
+	sed -i 's|www\.AaronBishopGames\.com|$(GIT_REMOTE_URL)|' $(DATADIR)/WMAIN.SRC; \
+	sed -i 's|__YEAR__|$(BUILD_YEAR)|' $(DATADIR)/WMAIN.SRC; \
+	sed -i 's|__GIT_VERSION__|$(GIT_VERSION)|' $(DATADIR)/WMAIN.SRC; \
+	$(SSC) -c -i $(DATADIR); \
+	SSC_EXIT=$$?; \
+	cp $$TMPBAK $(DATADIR)/WMAIN.SRC; \
+	rm -f $$TMPBAK; \
+	touch -d "$$ORIG_MTIME" $(DATADIR)/WMAIN.SRC; \
+	exit $$SSC_EXIT
 
 $(SDP): | sdp
 	make -C sdp
